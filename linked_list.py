515c7c224b09190f73533a09ad316ea1cf24b926
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-06 14:58:59 +0200

    Update: Added text comments, headers for new functions and fixed insertion to constant time.

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,11 @@
+# Linked List
+# A linked list is a datastructure where each node holds a value and a reference to the next node. It is like a chain
+# where one can find a value through another node, traversing the chain until a value is found. The linked list is slow
+# when we either want to remove/find/add an element in the middle O(N), or if we want to remove the very last element. 
+# Remember, we cannot traverse backwards and to remove an element is like 'unhooking' its links, and so we must have the 
+# element before whatever element we want to remove. As such removing the last element is always of time complexity N. 
+# TODO: Implement insertions, deletions and find functions.
+
 class node:
     def __init__(self, val = None):
         self.value = val
@@ -6,19 +14,17 @@ class node:
 class linked_list:
     def __init__(self):
         self.bot_node = None
+        self.top_node = None
         self.size = 0
 
-    # Insertion is of O(n), we exhaust the entire list to get to the last val:
     def insertion(self, val):
         new_node = node(val)
         if(self.size > 0):
-            currentNode = self.bot_node
-            for i in range(1, self.size):
-                currentNode = currentNode.nextVal
-            currentNode.nextVal = new_node
+            self.top_node.nextVal = new_node
         else:
             self.bot_node = new_node
 
+        self.top_node = new_node
         self.size += 1
 
     # Element wise print of the linked list:
@@ -36,3 +42,16 @@ class linked_list:
         self.size -= 1
 
         return currentNode.value
+    
+    # TODO
+    def find_element(self):
+        pass
+    
+    def insert_on_index(self,val,i):
+        pass
+    
+    def remove_on_index(self, i):
+        pass
+    
+    def remove_last(self):
+        pass
