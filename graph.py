16a904140e7a1875a5aa2a758b2e9ab400fc3ad3
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-07-13 16:18:46 +0200

    Added peek()

--- graph.py
+++ graph.py
@@ -1,105 +1,32 @@
-# graph
-# A graph is a datastructure with a network of nodes that are glued together by edges. A graph can be either directed or
-# undirected. Directed means that one can access node2 from an edge emerging from node1, to node2. An undirected graph
-# also makes it so that such a directed edge is symmetric, if one can go from node2 from node 1, one can also go back to
-# node 1, from node 2. A graph is undirected by default as per the default value false for the parameter 'directed' below.
-# One can also set this to be true when creating the graph, making it so the edges are non-symmetric. The datastructure
-# can bind together a network of nodes and make typical graph searches such as DFS and BFS implemented by queues and
-# stacks specifically. Moreover there is a function to see if the graph is of a tree structure.
+# Queue
+# The queue is, one can say, the inverse of the stack. It is only possible to pop the first element rather than the last.
+# The queue is very easily conceptualized, it is just to close one's eyes and imagine oneself in a queue outside the
+# best carusel or attraction of one's child hood. The queue works exactly like a regular queue that is you push a value
+# and that value that is pushed to the queue kindly awaits its turn to enter. This is implemented using a linked list as
+# a linked list can easily remove the first element in constant time O(1) you only need a pointer for that. A generalized
+# graph traversal using a queue will amount to a BFS.
 
-import stack
-import queue
+import linked_list
+class queue:
+    def __init__(self):
+        self.linkedList = linked_list.linked_list()
+        self.size = 0
 
+    def push(self, element):
+        self.linkedList.insertion(element)
+        self.size += 1
 
-class node:
-    def __init__(self, name = None):
-        self.name = name
-        self.edges = []
+    def pop(self):
+        try:
+            pop_element = self.linkedList.remove_first()
+            self.size -= 1
 
-    def set_neighbors(self,adj_list):
-        for node in adj_list:
-            self.edges.append(node)
+            return pop_element
+        except:
+            print("You cannot pop from an empty queue!")
 
-    def __str__(self):
-        return self.name
+    def peek(self):
+        return self.linkedList.top_node.value
 
-class graph:
-    def __init__(self, start, directed = False):
-        self.start = start
-        self.node_list = []
-        self.adjacency_list = {}
-        self.add_node(start)
-        self.visited = {}
-        self.directed = directed
-
-    def add_node(self, node):
-        self.node_list.append(node)
-        self.adjacency_list[node] = []
-
-
-    def add_edge(self, node1,node2):
-        if(not self.directed):
-            self.adjacency_list[node1] += [node2]
-            self.adjacency_list[node2] += [node1]
-        else:
-            self.adjacency_list[node1] += [node2]
-
-    # print_edges()
-    # prints the adjacency list of the graph:
-    def print_edges(self):
-        print("ADJACENCY LIST:")
-        for entry in self.adjacency_list:
-            l = [e.__str__() for e in self.adjacency_list[entry]]
-            print("Entry " + entry.__str__() + ":", l)
-
-    # search()
-    # This is a generalized searching method, if one puts in structure = stack it will be a DFS search whereas if one
-    # inserts a structure = queue, this will instead be a BFS search. DFS and BFS down below both use this generalized
-    # search as a helper function, setting it up with DFS and BFS respectively.
-
-    def search (self, node, level, structure):
-        neighbors = structure
-        if(not self.visited.__contains__(node)):
-            print("Hello I am node:", node, "At search attempt:", level)
-            self.visited[node] = True
-            nodes = self.adjacency_list[node]       # Returns a list of neighbors.
-            for i in range (len(nodes)):
-                neighbors.push(nodes[i])            # Pushing neighbors.
-
-            while(neighbors.size > 0):
-                self.search(neighbors.pop(), level + 1, structure)
-
-    def DFS(self, node, level):
-        print("\nDEPTH FIRST SEARCH: ")
-        self.visited = {}
-        self.search(node, level, stack.stack())
-
-    def BFS(self, node, level):
-        print("\nBREADTH FIRST SEARCH: ")
-        self.visited = {}
-        self.search(node, level, queue.queue())
-
-    # isTree()
-    # A function that goes through the current adjacency list, looking for any cycles. Worth to note is that this is
-    # implemented with regard to an undirected graph
-
-    def isTree(self, start):
-        tree = True
-        for i in self.adjacency_list:
-            for j in self.adjacency_list:
-                if(i == j):
-                    continue
-                else:
-                    for n in self.adjacency_list[i]:
-                        if(n in self.adjacency_list[j] and j in self.adjacency_list[i]):
-                            print(n, self.adjacency_list[j])
-                            tree = False
-                            break
-
-                    if(not tree):
-                        break
-
-            if(not tree):
-                break
-
-        return tree
+    def element_wise_print(self):
+        self.linkedList.element_wise_print()
