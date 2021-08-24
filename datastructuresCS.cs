7f7f122fb633488fdbde96563bf332072ab41c52
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-24 06:00:43 +0200

    Initial stack and queue implementation.

--- datastructuresCS.cs
+++ datastructuresCS.cs
@@ -4,10 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 class stack{
-    // Class members:
+    // Data Members:
     List<int> internal_list = new List<int>();
     int elements = 0;
     
+    // Member Functions:
     public void push(int elem){
         internal_list.Add(elem);
         elements ++;
@@ -24,7 +25,49 @@ class stack{
         Console.WriteLine("Please don't pop an empty list!");
         return -1;
     }
+    
+    public bool contains(int elem){
+        return internal_list.Contains(elem);
+    }
+    
+    public int length(){
+        return elements;
+    }
 }
+
+class queue{
+    // Data Members:
+    List<int> internal_list = new List<int>();
+    int elements = 0;
+    
+    // Member Functions:
+    public void push(int elem){
+        internal_list.Add(elem);
+        elements ++;
+    }
+    
+    public int pop(){
+        if(elements > 0){
+            elements --;
+            int elem = internal_list[0];
+            internal_list.RemoveAt(0);
+            return elem;
+        }
+        
+        Console.WriteLine("Please don't pop an empty list!");
+        return -1;
+    }
+    
+    public bool contains(int elem){
+        return internal_list.Contains(elem);
+    }
+    
+    public int length(){
+        return elements;
+    }
+}
+
+
 
 class Program
 {
